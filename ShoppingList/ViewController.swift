601b1ff53e83fda1e3b99b17d5dda80cdbbc3c32
Mohammad <Moh.jalhaddad@gmail.com>
2022-01-12 01:36:26 +0300

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Mohammad Al-haddad on 12/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
